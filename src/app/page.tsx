f1b1fff87ad066dc003dcdc02e13dde198bcae53
Hook Tools <96314649+monleru@users.noreply.github.com>
2023-08-01 18:45:02 +0600

    first commint

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Coffee
+    </main>
+  )
+}
