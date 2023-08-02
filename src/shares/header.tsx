4fd25ec079bbc7cb7bc98c3d37af33c8497a761d
Hook Tools <96314649+monleru@users.noreply.github.com>
2023-08-03 01:01:38 +0600

    first commit

--- src/shares/header.tsx
+++ src/shares/header.tsx
@@ -8,12 +8,12 @@ export const Header = () => {
     <header className="fixed left-0 top 0 w-full flex items-center justify-between p-5 h-24">
       <p>Logo</p>
       <div className="flex items-center gap-8">
-        <Link href="/">All Coffe</Link>
+        <Link href="/">All Coffee</Link>
         <Link
           className="flex gap-2 text-black bg-slate-300 font-medium py-2 px-4 rounded-full"
           href="cart"
         >
-          <Image src={cart_icon} alt="cart" />
+          <Image src={cart_icon} width={16} height={17} alt="cart" />
           <p>cart</p>
         </Link>
       </div>
