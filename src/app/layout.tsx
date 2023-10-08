79c5afcfbb50d4f9542dd055fa51fcc43ed4552b
zufar-sunagatov <zufar.sunagatov@gmail.com>
2023-10-08 23:51:30 +0100

    layout.tsx was improved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import React from 'react'
-import { Header } from '../shares/header'
-import './globals.css'
+import React from 'react';
+import {Metadata} from 'next';
+import {Inter} from 'next/font/google';
+import {Header} from '../shares/header';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
-  title: 'Online store',
-  description: 'Online store',
+    title: 'Online store',
+    description: 'Online store',
+};
+
+interface RootLayoutProps {
+    children: React.ReactNode;
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
+export default function RootLayout({children}: RootLayoutProps) {
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <Header/>
         {children}
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    );
 }
