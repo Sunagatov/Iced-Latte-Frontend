4a4ff5c1128dab5f573140cba41b434a6c3562d2
Kiryl Sandryhaila <ksandryhaila.dev@gmail.com>
2023-10-31 18:00:30 +0300

    refactor: error handling in getAllProducts

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,5 +1,14 @@
 export async function getAllProducts(url: string) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST_REMOTE}/${url}`)
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST_REMOTE}/${url}`)
 
-  return res.json()
+    if (res.status !== 200) {
+      throw new Error(res.statusText)
+    }
+
+    return res.json()
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+    else throw new Error('Something went wrong')
+  }
 }
