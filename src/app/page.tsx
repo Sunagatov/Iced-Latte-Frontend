4a4455b67052670137f9536c4e259afae8371cfd
Kiryl Sandryhaila <ksandryhaila.dev@gmail.com>
2023-10-31 18:22:54 +0300

    Revert "refactor: move products list component to app/(products) folder"
    This reverts commit 2bedb44b4201fbd5da6506ac9adf0c198da1d972.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Products from './(products)/page'
+import ProductList from './_components/ProductList'
 
 export default function Home() {
   return (
     <>
       <nav className={'h-[98px] bg-black'}></nav>
-      <Products />
+      <h1 className={'text-6XL w-[1144px] m-auto'}>All Coffee</h1>
+      <ProductList />
     </>
   )
 }
