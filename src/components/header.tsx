b6de92c2ea4ca81d4f9e3a5e7a89c1d4885c9bec
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-10-31 17:32:59 +0100

    style(header): add mobile icon  adaptation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export const Header = () => {
   return (
     <section className="fixed left-0 top-0 w-full flex items-center bg-white justify-between pl-9 pr-6 h-24 mx-auto">
-      <div className="flex gap-4">
+      <div className="flex gap-3 items-center sm:gap-4">
         <Link href="/">
           <div className="w-[19px] h-[21px] sm:w-[28px] sm:h-[31px]">
             <Image src={logo} width={28} height={31} alt="Logo" priority />
@@ -15,13 +15,19 @@ export const Header = () => {
         <Link href="/" className="hidden items-center sm:flex ">
           Good Folks Roasters
         </Link>
-        <Link href="/" className="sm:hidden items-center flex">
+        <Link
+          href="/"
+          className="sm:hidden text-lg font-medium items-center text-slate-950 flex"
+        >
           GFR
         </Link>
       </div>
       <div>
         <button className="flex items-center gap-2 text-black sm:bg-slate-300 font-medium py-2 px-4 rounded-full">
-          <Image src={cart_icon} width={16} height={17} alt="Cart" priority />
+          <div className="w-[20px] h-[20px] sm:w-[16px] sm:h-[17px]">
+            <Image src={cart_icon} width={20} height={20} alt="Cart" priority />
+          </div>
+
           <p className="hidden items-center sm:flex ">Cart</p>
         </button>
       </div>
