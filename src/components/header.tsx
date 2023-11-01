474c777597271f5c14d42eeab1706688c8f59e5b
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-11-01 11:04:31 +0100

    add file _env.local to root

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,25 +5,25 @@ import Link from 'next/link'
 
 export const Header = () => {
   return (
-    <section className="fixed left-0 top-0 w-full flex items-center bg-white justify-between pl-9 pr-6 h-24 mx-auto">
+    <section className="fixed left-0 top-0 w-full flex items-center bg-primary justify-between pl-9 pr-6 h-24 mx-auto">
       <div className="flex gap-3 items-center sm:gap-4">
         <Link href="/">
           <div className="w-[19px] h-[21px] sm:w-[28px] sm:h-[31px]">
             <Image src={logo} width={28} height={31} alt="Logo" priority />
           </div>
         </Link>
-        <Link href="/" className="hidden items-center sm:flex ">
+        <Link href="/" className="hidden text-L items-center sm:flex ">
           Good Folks Roasters
         </Link>
         <Link
           href="/"
-          className="sm:hidden text-lg font-medium items-center text-slate-950 flex"
+          className="sm:hidden text-L font-medium items-center text-primary flex"
         >
           GFR
         </Link>
       </div>
       <div>
-        <button className="flex items-center gap-2 text-black sm:bg-slate-300 font-medium py-2 px-4 rounded-full">
+        <button className="flex items-center gap-2 text-primary sm:bg-secondary font-medium py-2 px-4 rounded-full">
           <div className="w-[20px] h-[20px] sm:w-[16px] sm:h-[17px]">
             <Image src={cart_icon} width={20} height={20} alt="Cart" priority />
           </div>
