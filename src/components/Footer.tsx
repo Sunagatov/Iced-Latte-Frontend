16a1ba997e0f657241ab549c0d6be0296968f70d
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-11-05 23:57:31 +0100

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+export default function Footer() {
+  return (
+    <footer
+      className={
+        'flex h-48 w-full items-center justify-start bg-black text-white sm:h-36 sm:flex-row'
+      }
+    >
+      <div className={' ml-6 text-lg sm:mx-36 sm:flex sm:gap-36'}>
+        <ul className={'flex flex-wrap sm:order-2 sm:flex-col'}>
+          <li>
+            <a href="tel:(555) 123-45675" rel="noopener noreferrer">
+              Phone Number: (555) 123-4567
+            </a>
+          </li>
+          <li>
+            Email:
+            <span className={'ml-1 underline  sm:no-underline'}>
+              <a
+                href="mailto:info@coffeetimecafe.com"
+                rel="noopener noreferrer"
+              >
+                info@coffeetimecafe.com
+              </a>
+            </span>
+          </li>
+        </ul>
+        <p className={'mt-6 sm:order-1 sm:mb-0 sm:mr-6'}>
+          © 2023 Good Folks Roasters
+        </p>
+      </div>
+    </footer>
+  )
+}
