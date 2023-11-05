c61d2a86ab361e238827acd60e991ee041946fab
Kiryl Sandryhaila <ksandryhaila.dev@gmail.com>
2023-11-06 01:22:49 +0300

    style: add margins to products title

--- src/app/_components/ProductList.tsx
+++ src/app/_components/ProductList.tsx
@@ -25,7 +25,13 @@ export default function ProductList() {
   return (
     <section className={'mb-5 mt-6 text-center min-[1124px]:mt-16'}>
       <div className={'inline-flex flex-col items-center text-left'}>
-        <h1 className={'mr-auto text-5XL min-[1124px]:text-6XL'}>All Coffee</h1>
+        <h1
+          className={
+            'mb-8 mr-auto text-5XL min-[1124px]:mb-10 min-[1124px]:text-6XL'
+          }
+        >
+          All Coffee
+        </h1>
         <ul
           className={
             'grid grid-cols-2 gap-x-[6px] gap-y-14 min-[1124px]:grid-cols-3 min-[1124px]:gap-x-8 min-[1124px]:gap-y-12'
