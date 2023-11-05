b67445af2ced8b3949ac59a33d6cccb3cad1ce16
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-11-05 17:04:55 +0100

    refactor(Hero,Header): change component naming, delete excessive div

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,17 +1,15 @@
 import Image from 'next/image'
 import hero from '../../../public/hero.png'
 
-export const Hero = () => {
+export default function Hero() {
   return (
     <section className={' mx-36 flex h-screen  justify-center '}>
-      <div>
-        <div
-          className={
-            'h-[200px] w-[572px] rounded-2xl sm:h-[400px] sm:w-[1144px] sm:rounded-none'
-          }
-        >
-          <Image src={hero} width={1144} height={400} alt="Hero" priority />
-        </div>
+      <div
+        className={
+          'h-[200px] w-[572px] rounded-2xl sm:h-[400px] sm:w-[1144px] sm:rounded-none'
+        }
+      >
+        <Image src={hero} width={1144} height={400} alt="Hero" priority />
       </div>
     </section>
   )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import CartButton from './CartButton'
 
-export const Header = () => {
+export default function Header() {
   return (
     <section
       className={
