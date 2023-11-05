4687b1ab8b6d793a31956658d2753e1f33c33d19
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-11-05 15:24:15 +0100

    fix(Hero): conditional redering of hero section is  working correctly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
-'use client'
-
 import React from 'react'
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/Header'
-import { Hero } from '@/components/Hero'
-import { useRouter } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,18 +10,17 @@ export const metadata: Metadata = {
   title: 'Online store',
   description: 'Online store',
 }
+
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const router = useRouter()
-
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {router.pathname === '/' && <Hero />}
+
         {children}
       </body>
     </html>
