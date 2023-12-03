7217b881b97f4f889cd2753083afc7107cffe64e
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-12-03 19:29:54 +0200

    create new branch

---
+++ src/app/profile/_components/FormProfile/FormProfile.tsx
@@ -0,0 +1,13 @@
+import Container from '@/components/Container/Container'
+
+const FormProfile = () => {
+  return (
+    <div>
+      <Container>
+        <div></div>
+      </Container>
+    </div>
+  )
+}
+
+export default FormProfile
