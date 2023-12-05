490df1ae44b42b50f6a1d39074d2ff308df936df
Kiryl Sandryhaila <ksandryhaila.dev@gmail.com>
2023-12-05 23:17:08 +0300

    feature: connection with cart store logic

--- src/app/cart/_components/CartFull.tsx
+++ src/app/cart/_components/CartFull.tsx
@@ -5,25 +5,30 @@ import { CartItem } from '@/store/cartSlice'
 import Link from 'next/link'
 
 export default function CartFull() {
-  const { items, totalPrice, removeFullProduct, remove } = useCombinedStore()
+  const { items, totalPrice, removeFullProduct, remove, add } =
+    useCombinedStore()
 
   return (
-    <div className="w-{800px} 1px h-{513px} mx-auto px-4 sm:w-[500px]">
+    <div className="h-{513px} mx-auto flex min-w-[328px] flex-col px-4 md:max-w-[800px]">
       <h2 className="mx-4 my-6 text-left text-4xl">Shopping cart</h2>
-      {items.map((item: CartItem) => (
-        <CartElement
-          key={item.id}
-          product={item}
-          remove={remove}
-          removeAll={removeFullProduct}
-        />
-      ))}
+      <div>
+        {items.map((item: CartItem) => (
+          <CartElement
+            key={item.id}
+            product={item}
+            remove={remove}
+            removeAll={removeFullProduct}
+            add={add}
+          />
+        ))}
+      </div>
+
       <div className="mt-4 flex justify-between font-semibold ">
         <p>Subtotal:</p>
         <p className="">${totalPrice.toFixed(2)}</p>
       </div>
       <div className="flex justify-center">
-        <Button className="h-14 w-[211px] text-lg font-medium">
+        <Button className="my-6 h-14 w-full text-lg font-medium sm:w-[211px]">
           <Link href={'/'}>Go to checkout</Link>
         </Button>
       </div>
