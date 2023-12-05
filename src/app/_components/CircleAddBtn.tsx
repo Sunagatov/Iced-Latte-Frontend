c7c4ed4d770398bf27ff0a2bacaae6fce5219a01
Kiryl Sandryhaila <ksandryhaila.dev@gmail.com>
2023-12-05 23:08:41 +0300

    ref: refactor component props

--- src/app/_components/CircleAddBtn.tsx
+++ src/app/_components/CircleAddBtn.tsx
@@ -3,9 +3,9 @@
 import Image from 'next/image'
 import circle_btn from '../../../public/circle_btn.png'
 
-export default function CircleAddBtn() {
+export default function CircleAddBtn({ onClick }: { onClick: () => void }) {
   return (
-    <div>
+    <div onClick={onClick}>
       <Image src={circle_btn} alt="add to cart" className={'cursor-pointer'} />
     </div>
   )
