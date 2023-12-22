d6525260a8301ee0a38dde9460109f539f478d60
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-12-22 19:08:14 +0200

    added route to the main page on successful registration,isLoggedIn state wrapped in custom hook for asynchronous state processing ,made a check for clearing the setOpen state to reset the modal window state to false when clicking on the logo or going to the profile page, since it remains true until the page reloads.

--- src/components/LogicIcon.tsx
+++ src/components/LogicIcon.tsx
@@ -1,24 +1,40 @@
 'use client'
-import { useState } from 'react'
 import Image from 'next/image'
 import ProfileImage from '../../public/person.svg'
-import { AuthModal } from '@/components/modals/AuthModal'
 import Link from 'next/link'
+import { AuthModal } from '@/components/modals/AuthModal'
+import { useState, useEffect } from 'react'
 import { useAuthStore } from '@/store/authStore'
+import { useStoreData } from '@/hooks/useStoreData'
+import { usePathname } from 'next/navigation'
 
 export default function LoginIcon() {
   const [open, setOpen] = useState(false)
+  const pathname = usePathname()
   const toggleModal = () => setOpen((state) => !state)
-  const { isLoggedIn } = useAuthStore()
+  const isLoggedIn = useStoreData(
+    useAuthStore,
+    (state) => state.isLoggedIn,
+  ) as boolean
+
+  useEffect(() => {
+    // Close the modal window when navigating to the profile page because the open state remains true until the page is refreshed
+    if (pathname === '/profile' || pathname === '/') {
+      setOpen(false)
+    }
+  }, [pathname])
 
   return (
     <>
-      <Link
-        href={isLoggedIn ? '/profile' : '/auth/login'}
-        className="inline-flex"
-      >
-        <Image src={ProfileImage} alt="Profile" onClick={toggleModal} />
-      </Link>
+      {isLoggedIn ? (
+        <Link href="/profile" className="inline-flex">
+          <Image src={ProfileImage} alt="Profile icon" />
+        </Link>
+      ) : (
+        <Link href="/auth/login" className="inline-flex">
+          <Image src={ProfileImage} alt="Profile icon" onClick={toggleModal} />
+        </Link>
+      )}
       {open && <AuthModal onCloseModal={toggleModal} />}
     </>
   )
