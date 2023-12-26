1aeac381753f0872f60c293c03e829c02a6fd048
Sandryhaila Kiryl <ksandryhaila.dev@gmail.com>
2023-12-26 23:54:47 +0300

    ref: add active and focus effect

--- src/app/_components/CircleAddBtn.tsx
+++ src/app/_components/CircleAddBtn.tsx
@@ -8,7 +8,7 @@ export default function CircleAddBtn({ onClick }: { onClick: () => void }) {
     <div onClick={onClick}>
       <button
         className={
-          'flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-inverted hover:bg-fullpage-tint'
+          'flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-inverted hover:bg-fullpage-tint focus:bg-inverted active:bg-fullpage-tint'
         }
       >
         <Image src={circle_btn} alt="add to cart" />
