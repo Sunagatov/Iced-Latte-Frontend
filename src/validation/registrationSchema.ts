d7b8c659465595661fac8f4e761d1818ec652fa8
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-12-29 19:01:00 +0200

    fix validation schema

--- src/validation/registrationSchema.ts
+++ src/validation/registrationSchema.ts
@@ -1,34 +1,37 @@
 import * as yup from 'yup'
 
-const emailRegex =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
-
-const nameRegex = /^(?!\\s)(?!.*\\s$)(?!.*?--)[A-Za-z\\s-]*$/
-
 export const registrationSchema = yup.object().shape({
   firstName: yup
     .string()
-    .required('First name is a required field')
-    .min(2, 'First name should have a length between 2 and 128 characters')
-    .max(128, 'First name should have a length between 2 and 128 characters')
+    .min(4, 'Name should be at least 4 characters')
+    .max(64, 'Name should not exceed 64 characters')
     .matches(
-      nameRegex,
-      'First name can contain Latin letters, spaces, and hyphens',
-    ),
+      /^[a-zA-Zа-яА-ЯёЁ][a-zA-Zа-яА-ЯёЁ0-9.%+\-_]*( [a-zA-Zа-яА-ЯёЁ0-9.%+\-_]+)?$/,
+      'Invalid name format',
+    )
+    .required('name is required'),
   lastName: yup
     .string()
-    .required('Last name is a required field')
-    .min(2, 'Last name should have a length between 2 and 128 characters')
-    .max(128, 'Last name should have a length between 2 and 128 characters')
+    .min(4, 'last name should be at least 4 characters')
+    .max(64, 'last name should not exceed 64 characters')
     .matches(
-      nameRegex,
-      'Last name can contain Latin letters, spaces, and hyphens',
-    ),
+      /^[a-zA-Zа-яА-ЯёЁ][a-zA-Zа-яА-ЯёЁ0-9.%+\-_]*( [a-zA-Zа-яА-ЯёЁ0-9.%+\-_]+)?$/,
+      'Invalid last name format',
+    )
+    .required('last name is required'),
   email: yup
     .string()
-    .required('Email is a required field')
     .email('Invalid email')
-    .matches(emailRegex, 'Invalid email'),
+    .test(
+      'email-format',
+      'Invalid email format',
+      (value: string | undefined) => {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+        return emailRegex.test(value ?? '')
+      },
+    )
+    .required(),
   password: yup
     .string()
     .required('Password is a required field')
