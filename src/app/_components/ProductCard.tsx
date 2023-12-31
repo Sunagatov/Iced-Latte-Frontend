05837601e96d67823a48d3e01835b0af8762d811
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-12-31 11:38:33 +0100

    feat:add images

--- src/app/_components/ProductCard.tsx
+++ src/app/_components/ProductCard.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import star from '../../../public/star.png'
-import cardLogo from '../../../public/card_logo.png'
+import productImg from '../../../public/coffee.png'
 import CircleAddBtn from './CircleAddBtn'
 import { IProduct } from '@/models/Products'
 import { productRating, productSize } from '@/constants/product'
@@ -9,11 +9,12 @@ import { useCombinedStore } from '@/store/store'
 import { useAuthStore } from '@/store/authStore'
 import ButtonHeart from '@/components/Heart/ButtonHeart'
 import { useFavouritesStore } from '@/store/favStore'
+import createImgUrl from '@/utils/createImgUrl'
 
-type CardProps = Pick<IProduct, 'id' | 'name' | 'price' | 'description'>
+type CardProps = Pick<IProduct, 'id' | 'name' | 'price' | 'description' | 'productFileUrl'>
 
 
-export default function ProductCard({ id, name, price }: Readonly<CardProps>) {
+export default function ProductCard({ id, name, price, productFileUrl }: Readonly<CardProps>) {
 
   const addToCart = useCombinedStore((state) => state.add)
   const token = useAuthStore((state) => state.token)
@@ -32,17 +33,30 @@ export default function ProductCard({ id, name, price }: Readonly<CardProps>) {
     }
   }
 
+  if (!productFileUrl) {
+    console.log('image', productFileUrl)
+  }
+  console.log(productFileUrl)
+
   return (
     <div className={'relative flex w-[177px] flex-col gap-y-4 md:w-[360px]'}>
       <Link href={`/product/${id}`} className={'flex flex-col gap-y-4'}>
-        <Image src={cardLogo} alt="card picture" />
+        <div
+          className=' w-full h-[177px] md:h-[360px]  relative'>
+          <Image
+            src={createImgUrl(productFileUrl) ? productFileUrl : productImg}
+            alt="card picture"
+            style={{ objectFit: 'cover' }}
+            fill={true}
+          />
+        </div>
+
         <div className={'flex w-full flex-col gap-3'}>
           <h2 className={'text-L font-bold text-primary md:text-3XL'}>
             {name}
           </h2>
           <div className={' flex items-center gap-2 text-L md:text-2XL'}>
             <Image src={star} alt="star" className={'inline-block'} />
-
             <span>{productRating}</span>
             <span className={'text-placeholder'}>
               &#x2022; {productSize} g.
@@ -50,13 +64,11 @@ export default function ProductCard({ id, name, price }: Readonly<CardProps>) {
           </div>
         </div>
       </Link>
-
       <div className={' absolute right-0 top-0'}>
         <ButtonHeart active={isActive} onClick={handleButtonClick} className="ml-2" />
       </div>
       <div className={'flex items-end justify-between'}>
         <p className={'text-XL font-medium md:text-2XL'}>${price}</p>
-
         <CircleAddBtn
           onClick={() => {
             addToCart(id, token)
