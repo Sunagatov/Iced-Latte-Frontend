4ca4f314acf835689f1e9938a0974e59ef17c0d8
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-01-11 19:33:15 +0100

    ref: remove after sonarQ comments

--- src/utils/createImgUrl.ts
+++ src/utils/createImgUrl.ts
@@ -1,12 +1,3 @@
 export default function createImgUrl() {
   return null
 }
-
-// I kept this comment for the future whe
-// export default function createImgUrl(url: string | null): string | null {
-//   if (url === 'default file' || !url) {
-//     return null
-//   }
-
-//   return url
-// }
