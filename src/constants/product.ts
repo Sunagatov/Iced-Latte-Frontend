2a4862fda0d0578711e0509302e31716607910fa
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-01-11 20:29:09 +0100

    ref: delete mock data from constants

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -1,5 +1,2 @@
 export const productRating: string = '4,8'
 export const productSize: number = 500
-
-export const TEMP_MINIO_PIC_URL =
-  'https://unsplash.com/photos/shallow-focus-photography-of-coffee-late-in-mug-on-table-zUNs99PGDg0'
