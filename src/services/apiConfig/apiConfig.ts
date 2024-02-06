3f028c594a4ab5678747254dfaaeaee9ffad8a61
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-02-06 21:13:08 +0200

    update branch

--- src/services/apiConfig/apiConfig.ts
+++ src/services/apiConfig/apiConfig.ts
@@ -1,14 +1,3 @@
-// import axios from 'axios'
-
-// const baseConfig = {
-//   baseURL: process.env.NEXT_PUBLIC_API_HOST_REMOTE,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// }
-
-// export const api = axios.create(baseConfig)
-
 import axios from 'axios'
 import { setupCache } from 'axios-cache-interceptor'
 
--- src/utils/getImgUrl.ts
+++ src/utils/getImgUrl.ts
@@ -3,14 +3,14 @@ import { StaticImageData } from 'next/image'
 const DEFAULT_STATIC_FILE_URL_VALUE = 'default file'
 
 export default function getImgUrl(
-  staticFileUrl: string | null,
-  localImage: StaticImageData,
+  staticFileUrl: string | null | undefined,
+  localImage: StaticImageData | string,
 ): string | StaticImageData {
   if (
     typeof staticFileUrl === 'string' &&
     staticFileUrl != DEFAULT_STATIC_FILE_URL_VALUE
   ) {
-    return staticFileUrl
+    // return staticFileUrl
   }
 
   return localImage
