9530341ecbebbd805e4d9e73d0d9892a351fdba9
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-03-11 17:02:07 +0100

    fix:sonar error 2

--- src/validation/changePassSchema.ts
+++ src/validation/changePassSchema.ts
@@ -23,15 +23,18 @@ export const changePassSchema = yup.object().shape({
 export const authChangePassSchema = yup.object().shape({
   oldPassword: yup
     .string()
-    .required('Password is a required field')
-    .min(8, 'Password should have a length between 8 and 128 characters')
-    .max(128, 'Password should have a length between 8 and 128 characters')
+    .required('Old Password is a required field')
+    .min(8, 'Old Password should have a length between 8 and 128 characters')
+    .max(128, 'Old Password should have a length between 8 and 128 characters')
     .matches(
       /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z@$!%*?&]{8,}$/,
-      'Password should contain at least 1 letter, 1 digit, and may include special characters "@$!%*?&"',
+      'Old Password should contain at least 1 letter, 1 digit, and may include special characters "@$!%*?&"',
     ),
   newPassword: yup
     .string()
-    .oneOf([yup.ref('password')], 'Passwords must match')
-    .required('Please confirm your password'),
+    .oneOf(
+      [yup.ref('oldPassword')],
+      'New Password must not be the same as Old Password',
+    )
+    .required('New Password is a required field'),
 })
