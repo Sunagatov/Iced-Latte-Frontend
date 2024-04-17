f842e88840d014d6c109d873dde33d16d4ac3812
Mikhail Saburov <saburov1985@gmail.com>
2024-04-17 21:26:38 +1100

    create file component Label

--- src/components/Product/ProductsFilterLabels/ProductsFilterLabels.tsx
+++ src/components/Product/ProductsFilterLabels/ProductsFilterLabels.tsx
@@ -1,7 +1,21 @@
 'use client'
 
+const filterAttributes = [
+  { id: 1, name: 'Brand1', label: 'label-1' },
+  { id: 2, name: 'Seller1', label: 'label-2' },
+  { id: 3, name: 'Seller5', label: 'label-3' }
+]
+
+
 export default function ProductsFilterLabels() {
   return (
-    <div>Label</div>
+    <div>
+      <button
+        className='text-white bg-black text-lg px-6  rounded-full w-[136px] h-[48px]'
+        onClick={() => { alert('Кнопку нажали') }}
+      >
+        By default
+      </button>
+    </div >
   )
 }
