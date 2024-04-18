e85db7a35f38c6b6fa3f019a12839b5312160a7d
freecree <bohdansidorov@gmail.com>
2024-04-18 20:29:02 +0300

    fix: remove checkbox handler, unnecessary styles

--- src/components/UI/Checkbox/Checkbox.tsx
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -3,18 +3,14 @@ import { PropsCheckbox } from '@/types/Checkbox'
 
 export default function Checkbox({ label, isChecked, onCheck }: PropsCheckbox) {
 
-  const handleOnChange = () => {
-    onCheck()
-  }
-
   return (
     <>
       <label className="inline-flex items-center">
         <input
           type="checkbox"
-          className="h-[24px] w-[24px] cursor-pointer hover:bg-hover-heart rounded bg-secondary border-none focus:ring-0 focus:ring-offset-0 focus-visible:ring-focus checked:bg-inverted focus:checked:bg-inverted hover:checked:bg-inverted"
+          className="h-6 w-6 cursor-pointer hover:bg-hover-heart rounded bg-secondary border-none focus:ring-0 focus:ring-offset-0 checked:bg-inverted focus:checked:bg-inverted hover:checked:bg-inverted"
           checked={isChecked}
-          onChange={handleOnChange}
+          onChange={onCheck}
         />
         {label && <span className="ml-[16px] text-primary text-L font-medium">{label}</span>}
       </label>
