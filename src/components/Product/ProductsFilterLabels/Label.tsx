d5ba4850a95ba6a87004e007ee63cb7ed0a0d81c
Mikhail Saburov <saburov1985@gmail.com>
2024-04-19 23:57:52 +1100

    LabelProps renamed to Ilabel

--- src/components/Product/ProductsFilterLabels/Label.tsx
+++ src/components/Product/ProductsFilterLabels/Label.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-export interface LabelProps {
+interface Ilabel {
   id: string
   name: string
 }
 
-export default function Label({ name, id }: LabelProps) {
+export default function Label({ name, id }: Ilabel) {
   return (
     <button className='text-white bg-black text-lg px-6  rounded-full w-[136px] h-[48px] flex items-center gap-3'>
       <span className='cursor-pointer' onClick={() => { alert(`click label ${id}`) }}><svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
