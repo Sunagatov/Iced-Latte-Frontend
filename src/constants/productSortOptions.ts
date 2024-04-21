04d2998d372d5dfaa8f2fd1f3fde93c2852bb210
Lightness322 <dyubkov99@mail.ru>
2024-04-21 13:42:33 +0500

    add implemented on backend sort options

--- src/constants/productSortOptions.ts
+++ src/constants/productSortOptions.ts
@@ -1,23 +1,38 @@
 export const sortOptions = [
   {
-    label: 'Price: Low to High',
-    value: { sortAttribute: 'price', sortDirection: 'asc' },
-  },
-  {
     label: 'Price: High to Low',
     value: { sortAttribute: 'price', sortDirection: 'desc' },
   },
   {
+    label: 'Price: Low to High',
+    value: { sortAttribute: 'price', sortDirection: 'asc' },
+  },
+  {
     label: 'High rating first',
-    value: { sortAttribute: '', sortDirection: 'desc' }, //not implemented on the backend yet
+    value: { sortAttribute: 'averageRating', sortDirection: 'desc' },
   },
   {
     label: 'Low rating first',
-    value: { sortAttribute: '', sortDirection: 'asc' }, //not implemented on the backend yet
+    value: { sortAttribute: 'averageRating', sortDirection: 'asc' },
+  },
+  {
+    label: 'Quantity: High to Low',
+    value: { sortAttribute: 'quantity', sortDirection: 'desc' },
+  },
+  {
+    label: 'Quantity: Low to High',
+    value: { sortAttribute: 'quantity', sortDirection: 'asc' },
+  },
+  {
+    label: 'Reviews: High to Low',
+    value: { sortAttribute: 'reviewsCount', sortDirection: 'desc' },
+  },
+  {
+    label: 'Reviews: Low to High',
+    value: { sortAttribute: 'reviewsCount', sortDirection: 'asc' },
   },
   {
     label: 'Best Sellers',
     value: { sortAttribute: '', sortDirection: 'asc' }, //not implemented on the backend yet
   },
 ] as const
- 
