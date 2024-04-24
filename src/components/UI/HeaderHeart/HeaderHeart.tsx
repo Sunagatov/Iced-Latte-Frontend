632cba9ace1341fa6663035dcd5e58ee97b4989c
Lightness322 <dyubkov99@mail.ru>
2024-04-24 14:52:40 +0500

    change styles for favourite button in header

--- src/components/UI/HeaderHeart/HeaderHeart.tsx
+++ src/components/UI/HeaderHeart/HeaderHeart.tsx
@@ -3,26 +3,16 @@ import Link from 'next/link'
 import Image from 'next/image'
 import heart_black from '../../../../public/heart_black.svg'
 import heart_purple from '../../../../public/heart_purple.svg'
-
-import { useFavouritesStore } from '@/store/favStore'
+import { usePathname } from 'next/navigation'
 
 export default function HeaderHeart() {
-  const { favouriteIds } = useFavouritesStore()
+  const pathname = usePathname()
+
+  const imageUrl = pathname === '/favourites' ? heart_purple : heart_black
 
   return (
-    <Link href={'/favourites'}>
-      <button
-        className={
-          'relative flex items-center gap-2 rounded-full px-4 py-2 font-medium text-primary'
-        }
-      >
-        <div className={'h-[24px] w-[24px] sm:h-[24px] sm:w-[24px]'}>
-          <Image
-            src={favouriteIds.length > 0 ? heart_purple : heart_black}
-            alt={'heart'}
-          />
-        </div>
-      </button>
+    <Link className="inline-flex" href={'/favourites'}>
+      <Image src={imageUrl} alt={'heart'} />
     </Link>
   )
 }
