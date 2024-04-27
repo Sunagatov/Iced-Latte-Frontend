86ce2d2e77a55d646a489631362ab146bd125c5c
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-04-27 11:17:08 +0200

    chore: add pointer events

--- src/components/UI/Counter/Counter.tsx
+++ src/components/UI/Counter/Counter.tsx
@@ -41,7 +41,11 @@ const Counter = ({
 
   return (
     <div className={computedStyles}>
-      <button id="min-btn" onClick={onMinus}>
+      <button
+        id="min-btn"
+        onClick={onMinus}
+        className={count === 1 ? 'pointer-events-none opacity-10' : ''}
+      >
         <Image src={theme === 'dark' ? minus : minusDark} alt="minus" />
       </button>
       <span className={'block w-[31px] text-center'}>{count}</span>
