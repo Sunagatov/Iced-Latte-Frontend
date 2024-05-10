25f8b3a410c0c765cadbf4b5a6f3f41a32c562d7
Lightness322 <dyubkov99@mail.ru>
2024-05-11 02:02:27 +0500

    add async to Home.test

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -2,7 +2,9 @@ import { render } from '@testing-library/react'
 import Home from '@/app/page'
 import '@testing-library/jest-dom'
 
-it('Should pass', () => {
-  render(<Home />)
+it('Should pass', async () => {
+  const AwaitedHome = await Home()
+
+  render(AwaitedHome)
   expect(1).toBe(1)
 })
