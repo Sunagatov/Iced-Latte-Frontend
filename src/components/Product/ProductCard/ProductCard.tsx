5e734ad02f60a788bf90e4ab4846e24b63bdc564
Lightness322 <dyubkov99@mail.ru>
2024-06-08 20:55:21 +0300

    change refer to the new design

--- src/components/Product/ProductCard/ProductCard.tsx
+++ src/components/Product/ProductCard/ProductCard.tsx
@@ -4,20 +4,35 @@ import productImg from '../../../../public/coffee.png'
 import CircleAddBtn from '../../UI/Buttons/CircleAddBtn/CircleAddBtn'
 import getImgUrl from '@/utils/getImgUrl'
 import ButtonHeart from '@/components/UI/Heart/ButtonHeart'
-import { productSize } from '@/constants/product'
 import { useCombinedStore } from '@/store/store'
 import { useAuthStore } from '@/store/authStore'
 import { useFavouritesStore } from '@/store/favStore'
 import { ICardProps } from '@/types/ProductCard'
 import { handleFavouriteButtonClick } from '@/utils/favUtils'
 import ProductRating from '@/components/Product/ProductRating/ProductRating'
+import Counter from '@/components/UI/Counter/Counter'
 
 export default function ProductCard({ product }: Readonly<ICardProps>) {
-  const { id, name, productFileUrl, price, averageRating, reviewsCount } =
-    product
+  const {
+    id,
+    name,
+    productFileUrl,
+    price,
+    averageRating,
+    reviewsCount,
+    brandName,
+  } = product
 
+  const cartItems = useCombinedStore((state) => state.itemsIds)
   const addToCart = useCombinedStore((state) => state.add)
+  const removeFromCart = useCombinedStore((state) => state.remove)
+
+  const productCartQuantity = cartItems?.find(
+    (cartItem) => cartItem.productId === id,
+  )?.productQuantity
+
   const token = useAuthStore((state) => state.token)
+
   const { addFavourite, removeFavourite, favourites, favouriteIds } =
     useFavouritesStore()
 
@@ -37,14 +52,15 @@ export default function ProductCard({ product }: Readonly<ICardProps>) {
   }
 
   return (
-    <div
+    <li
       className={
-        'relative flex h-full flex-col justify-between gap-y-4 md:w-[346px]'
+        'relative flex w-full max-w-[225px] flex-col justify-between justify-self-center rounded-lg border border-secondary transition-all hover:-translate-y-2 hover:shadow-[9px_9px_5px_0px_#D2D2D31A] sm:min-w-[178px]'
       }
     >
       <Link href={`/product/${id}`} className={'flex flex-col gap-y-4'}>
-        <div className="relative aspect-square w-full md:h-[360px]">
+        <div className="relative aspect-[calc(225/188)] sm:h-[188px]">
           <Image
+            className="rounded-t-lg"
             src={getImgUrl(productFileUrl, productImg)}
             alt="card picture"
             style={{ objectFit: 'cover' }}
@@ -55,34 +71,45 @@ export default function ProductCard({ product }: Readonly<ICardProps>) {
             priority={true}
           />
         </div>
-
-        <div className={'flex w-full flex-col gap-3'}>
-          <h2 className={'text-L font-bold text-primary md:text-3XL'}>
-            {name}
-          </h2>
-          <div className={'flex items-center gap-2 text-M font-medium'}>
-            <ProductRating rating={averageRating} reviewsCount={reviewsCount} />
-            <span className={'text-placeholder'}>
-              &nbsp; &#x2022; &nbsp; {productSize} g.
-            </span>
+        <div className="flex flex-col gap-3.5 px-2.5 pb-3.5">
+          <ProductRating rating={averageRating} reviewsCount={reviewsCount} />
+          <div className={'flex w-full flex-col gap-1'}>
+            <h2 className={'text-[12px] font-bold text-primary sm:text-L'}>
+              {name}
+            </h2>
+            <p className="text-[10px] text-primary sm:text-XS">
+              by {brandName}
+            </p>
+            <p className="text-[10px] text-secondary sm:text-XS">Iced latte</p>
           </div>
         </div>
       </Link>
-      <div className={' absolute right-0 top-0'}>
-        <ButtonHeart
-          active={token ? isInFavourites : isActive}
-          onClick={handleButtonClick}
-          className="ml-2"
-        />
+      <hr className="h-[1px] w-full bg-secondary" />
+      <div className={'flex items-center justify-between px-2.5 py-3.5'}>
+        <p className={'text-M font-bold sm:text-L'}>${price}</p>
+        {productCartQuantity ? (
+          <Counter
+            theme="light"
+            className={'h-8 w-[84px] gap-2 text-[12px] sm:text-M'}
+            count={productCartQuantity}
+            removeProduct={() => removeFromCart(id, token)}
+            addProduct={() => addToCart(id, token)}
+          />
+        ) : (
+          <CircleAddBtn
+            className="h-8 w-8 bg-brand-solid hover:bg-brand-solid-hover focus:bg-brand-solid active:bg-brand-solid sm:h-10 sm:w-10"
+            iconClassName="h-3 w-3 sm:h-4 sm:w-4"
+            onClick={() => {
+              addToCart(id, token)
+            }}
+          />
+        )}
       </div>
-      <div className={'flex items-end justify-between'}>
-        <p className={'text-XL font-medium md:text-2XL'}>${price}</p>
-        <CircleAddBtn
-          onClick={() => {
-            addToCart(id, token)
-          }}
-        />
-      </div>
-    </div>
+      <ButtonHeart
+        active={token ? isInFavourites : isActive}
+        onClick={handleButtonClick}
+        className="absolute right-1 top-1 m-0 h-6 w-6 border-none bg-transparent p-1 sm:h-12 sm:w-12 sm:p-2"
+      />
+    </li>
   )
 }
