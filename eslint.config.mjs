7691c4689fb701fff4294fe3e36e70baabe8c099
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-02-21 02:19:23 +0000

    chore: upgrade Next.js v14→v16, ESLint v8→v9, migrate to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({ baseDirectory: __dirname })
+
+export default [
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ),
+  {
+    plugins: { '@typescript-eslint': tsPlugin },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 12,
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      indent: ['error', 2],
+      quotes: ['error', 'single', { allowTemplateLiterals: true }],
+      semi: ['error', 'never'],
+      'func-style': 0,
+      'max-len': 0,
+      'no-magic-numbers': 0,
+      'max-lines-per-function': 0,
+      'space-before-function-paren': ['error', { anonymous: 'never', named: 'never', asyncArrow: 'always' }],
+      'function-call-argument-newline': 0,
+      'padded-blocks': 0,
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      ],
+      'object-curly-spacing': ['error', 'always'],
+      'one-var': ['error', 'never'],
+      'quote-props': 0,
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: { attributes: false } }],
+    },
+  },
+  {
+    ignores: ['.next/**', 'node_modules/**'],
+  },
+]
