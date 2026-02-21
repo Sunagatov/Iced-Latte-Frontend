5ee2b7ea6aad1e3698f533cd5cb1cea36c222fd4
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-02-21 02:02:23 +0000

    Refactor: simplify cart store wrapper, delete unused useStoreData hook

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,9 +4,7 @@ import { type CartSliceStore, createCartSlice } from './cartSlice'
 
 export const useCombinedStore = create<CartSliceStore>()(
   persist(
-    (...a) => ({
-      ...createCartSlice(...a),
-    }),
+    createCartSlice,
     {
       name: 'cart-storage',
       partialize: (state) => ({
