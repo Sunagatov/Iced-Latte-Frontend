e722f458ff54c5a413ceb9d72f089ae508c0c763
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-03-04 00:12:54 +0000

    fix: pass recommendedConfig to FlatCompat to silence ESLint constructor warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
 import tsPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const compat = new FlatCompat({ baseDirectory: __dirname })
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: js.configs.recommended })
 
 export default [
   ...compat.extends(
